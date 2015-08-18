f318a0b256c789dd4ce6f72321f431bc7967ac1b
Szabolcs Ban <szabolcs.ban@wizguild.com>
2015-08-18 23:20:22 +0100

    fix Pebble Time issues + day of month is bigger

--- src/pebble_one.c
+++ src/pebble_one.c
@@ -24,7 +24,6 @@
  */
 
 #include <pebble.h>
-#include <time.h>
 
 #define CENTER_X    71
 #define CENTER_Y    71
@@ -32,11 +31,8 @@
 #define DOTS_SIZE    4
 #define HOUR_RADIUS 40
 #define MIN_RADIUS  60
-#define SEC_RADIUS  62
 
 static TextLayer *battp_layer;
-#define BATTP_BUFFER_BYTES 32
-static char battp_buffer[BATTP_BUFFER_BYTES];
 
 #define max(a,b) ((a) > (b) ? (a) : (b))
 #define min(a,b) ((a) < (b) ? (a) : (b))
@@ -44,7 +40,9 @@ static char battp_buffer[BATTP_BUFFER_BYTES];
 static Window *window;
 static Layer *background_layer;
 static Layer *hands_layer;
-static Layer *date_layer;
+static TextLayer *time_layer; 
+static TextLayer *date_layer; 
+static TextLayer *day_layer; 
 
 static GBitmap *logo;
 static BitmapLayer *logo_layer;
@@ -59,10 +57,11 @@ static struct tm *now = NULL;
 static int date_wday = -1;
 static int date_mday = -1;
 static bool was_connected = true;
+static int min;
+static int hour;
 
 static GFont *font;
-#define DATE_BUFFER_BYTES 32
-static char date_buffer[DATE_BUFFER_BYTES];
+static GFont *font2;
 
 const GPathInfo HOUR_POINTS = {
   8,
@@ -106,19 +105,6 @@ const GPathInfo MIN_POINTS = {
 };
 static GPath *min_path;
 
-const GPathInfo SEC_POINTS = {
-  4,
-  (GPoint []) {
-    { 2,  0},
-    { 2,-61},
-    {-2,-61},
-    {-2,  0},
-  }
-};
-static GPath *sec_path;
-
-const char WEEKDAY_NAMES[7][5] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
-
 const char TIMECHARS[12][2] = {"4","5","6","7","8","9","10","11","12","1","2","3"};
 
 static void background_layer_update_callback(Layer *layer, GContext* ctx) {
@@ -145,18 +131,13 @@ static void background_layer_update_callback(Layer *layer, GContext* ctx) {
 }
 
 static void hands_layer_update_callback(Layer *layer, GContext* ctx) {
-#if SCREENSHOT
-  now->tm_hour = 10;
-  now->tm_min = 9;
-  now->tm_sec = 36;
-#endif
 
   GPoint center = GPoint(CENTER_X, CENTER_Y);
 
   // hours and minutes
 //  int32_t hour_angle = TRIG_MAX_ANGLE * (now->tm_hour * 5 + now->tm_min / 12) / 60;
-  int32_t hour_angle = (TRIG_MAX_ANGLE * (((now->tm_hour % 12) * 6) + (now->tm_min / 10))) / (12 * 6);
-  int32_t min_angle = TRIG_MAX_ANGLE * now->tm_min / 60;
+  int32_t hour_angle = (TRIG_MAX_ANGLE * (((hour % 12) * 6) + (min / 10))) / (12 * 6);
+  int32_t min_angle = TRIG_MAX_ANGLE * min / 60;
   gpath_rotate_to(hour_path, hour_angle);
   gpath_rotate_to(hour_in_path, hour_angle);
   gpath_rotate_to(min_path, min_angle);
@@ -177,54 +158,28 @@ static void hands_layer_update_callback(Layer *layer, GContext* ctx) {
   graphics_fill_circle(ctx, center, DOTS_SIZE);
 }
 
-static void date_layer_update_callback(Layer *layer, GContext* ctx) {
-
-#if SCREENSHOT
-  now->tm_wday = 0;
-  now->tm_mday = 25;
-#endif
-
-  graphics_context_set_text_color(ctx, GColorWhite);
-  GRect info_rect = GRect(0, -2, 144, 32);
-  // weekday
-  //strftime(date_buffer, DATE_BUFFER_BYTES, "%a", now);
-  graphics_draw_text(ctx,
-    WEEKDAY_NAMES[now->tm_wday],
-    font,
-    info_rect,
-    GTextOverflowModeWordWrap,
-    GTextAlignmentLeft,
-    NULL);
-
-  static char s_time_text[] = "00:00"; 
-  strftime(s_time_text, sizeof(s_time_text), "%R", now);
-  graphics_draw_text(ctx,
-    s_time_text,
-    font,
-    info_rect,
-    GTextOverflowModeWordWrap,
-    GTextAlignmentCenter,
-    NULL);
-
-  // day of month
-  strftime(date_buffer, DATE_BUFFER_BYTES, "%e", now);
-  graphics_draw_text(ctx,
-    date_buffer,
-    font,
-    info_rect,
-    GTextOverflowModeWordWrap,
-    GTextAlignmentRight,
-    NULL);
-
-  date_wday = now->tm_wday;
-  date_mday = now->tm_mday;
-}
-
 static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
   now = tick_time;
+  min = tick_time->tm_min;
+  hour = tick_time->tm_hour;
   layer_mark_dirty(hands_layer);
-  if (now->tm_wday != date_wday || now->tm_mday != date_mday) {
-    layer_mark_dirty(date_layer);
+
+  static char time_text[] = "00:00"; 
+  strftime(time_text, sizeof(time_text), "%R", tick_time);
+  text_layer_set_text(time_layer, time_text);
+
+  if (tick_time->tm_wday != date_wday || tick_time->tm_mday != date_mday) {
+
+    static char date_text[] = "01"; 
+    strftime(date_text, sizeof(date_text), "%d", tick_time);
+    text_layer_set_text(date_layer, date_text);
+
+    static char day_text[] = "Wed"; 
+    strftime(day_text, sizeof(day_text), "%a", tick_time);
+    text_layer_set_text(day_layer, day_text);
+    date_mday = tick_time->tm_mday;
+    date_wday = tick_time->tm_wday;
+
   }
 }
 
@@ -254,7 +209,8 @@ static void lost_connection_warning(void *data) {
 }
 
 static void handle_battery(BatteryChargeState charge_state) {
-  snprintf(battp_buffer, BATTP_BUFFER_BYTES, "%s%d%%",  charge_state.is_charging ? "+" : "", charge_state.charge_percent);
+  static char battp_buffer [] = "+100%";
+  snprintf(battp_buffer, sizeof(battp_buffer), "%s%d%%",  charge_state.is_charging ? "+" : "", charge_state.charge_percent);
   text_layer_set_text(battp_layer, battp_buffer);
   bitmap_layer_set_bitmap(battery_layer, battery_images[
     (charge_state.is_charging ? 11 : 0) + min(charge_state.charge_percent / 10, 10)]);
@@ -266,24 +222,21 @@ static void handle_init() {
   window = window_create();
   window_stack_push(window, true /* Animated */);
   window_set_background_color(window, GColorBlack);
-
-  date_layer = layer_create(GRect(0, 144, 144, 24));
-  layer_set_update_proc(date_layer, &date_layer_update_callback);
-  layer_add_child(window_get_root_layer(window), date_layer);
+  Layer *window_layer = window_get_root_layer(window);
 
   background_layer = layer_create(GRect(0, 0, 144, 144));
-  layer_set_update_proc(background_layer, &background_layer_update_callback);
-  layer_add_child(window_get_root_layer(window), background_layer);
+  layer_set_update_proc(background_layer, background_layer_update_callback);
+  layer_add_child(window_layer, background_layer);
 
   hands_layer = layer_create(layer_get_frame(background_layer));
-  layer_set_update_proc(hands_layer, &hands_layer_update_callback);
+  layer_set_update_proc(hands_layer, hands_layer_update_callback);
   layer_add_child(background_layer, hands_layer);
 
   for (int i = 0; i < 22; i++) {
     battery_images[i] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BATTERY_0 + i);  
   }
   battery_layer = bitmap_layer_create(GRect(144-16-3, 3, 16, 10));
-  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(battery_layer));
+  layer_add_child(window_layer, bitmap_layer_get_layer(battery_layer));
 
   for (int i = 0; i < 2; i++)
     bluetooth_images[i] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BLUETOOTH_OFF + i);  
@@ -291,11 +244,9 @@ static void handle_init() {
   layer_add_child(background_layer, bitmap_layer_get_layer(bluetooth_layer));
 
   battp_layer = text_layer_create(GRect(0, 0, 32, 16));
-  strcpy(battp_buffer, "");
-  text_layer_set_text(battp_layer, battp_buffer);
   text_layer_set_text_color(battp_layer, GColorWhite);
   text_layer_set_background_color(battp_layer, GColorBlack);
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(battp_layer));
+  layer_add_child(window_layer, text_layer_get_layer(battp_layer));
   
   hour_path = gpath_create(&HOUR_POINTS);
   hour_in_path = gpath_create(&HOUR_IN_POINTS);
@@ -303,12 +254,32 @@ static void handle_init() {
   gpath_move_to(hour_in_path, GPoint(CENTER_X, CENTER_Y));
   min_path = gpath_create(&MIN_POINTS);
   gpath_move_to(min_path, GPoint(CENTER_X, CENTER_Y));
-  sec_path = gpath_create(&SEC_POINTS);
-  gpath_move_to(sec_path, GPoint(CENTER_X, CENTER_Y));
 
   font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_20));
+  font2 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_28));
 
-  tick_timer_service_subscribe(MINUTE_UNIT, &handle_tick);
+  time_layer = text_layer_create(GRect(25,146,94,24));
+  text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
+  text_layer_set_text_color(time_layer, GColorWhite);
+  text_layer_set_background_color(time_layer, GColorClear);
+  text_layer_set_font(time_layer, font);
+  layer_add_child(window_layer, text_layer_get_layer(time_layer));
+  
+  date_layer = text_layer_create(GRect(94, 136, 50, 50));
+  text_layer_set_text_alignment(date_layer, GTextAlignmentRight);
+  text_layer_set_text_color(date_layer, GColorWhite);
+  text_layer_set_background_color(date_layer, GColorClear);
+  text_layer_set_font(date_layer, font2);
+  layer_add_child(window_layer, text_layer_get_layer(date_layer));
+
+  day_layer = text_layer_create(GRect(0, 146, 50, 24));
+  text_layer_set_text_alignment(day_layer, GTextAlignmentLeft);
+  text_layer_set_text_color(day_layer, GColorWhite);
+  text_layer_set_background_color(day_layer, GColorClear);
+  text_layer_set_font(day_layer, font);
+  layer_add_child(window_layer, text_layer_get_layer(day_layer));
+
+  tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
   battery_state_service_subscribe(&handle_battery);
   handle_battery(battery_state_service_peek());
   bluetooth_connection_service_subscribe(&handle_bluetooth);
@@ -320,13 +291,14 @@ static void handle_deinit() {
   battery_state_service_unsubscribe();
   tick_timer_service_unsubscribe();
   fonts_unload_custom_font(font);
-  gpath_destroy(sec_path);
+  fonts_unload_custom_font(font2);
   gpath_destroy(min_path);
   gpath_destroy(hour_path);
   gpath_destroy(hour_in_path);
-#if DEBUG
-  text_layer_destroy(debug_layer);
-#endif
+  text_layer_destroy(battp_layer);
+  text_layer_destroy(date_layer);
+  text_layer_destroy(day_layer);
+  text_layer_destroy(time_layer);
   layer_destroy(hands_layer);
   bitmap_layer_destroy(logo_layer);
   gbitmap_destroy(logo);
@@ -337,7 +309,6 @@ static void handle_deinit() {
   for (int i = 0; i < 2; i++)
     gbitmap_destroy(bluetooth_images[i]);
   layer_destroy(background_layer);
-  layer_destroy(date_layer);
   window_destroy(window);
 }
 
